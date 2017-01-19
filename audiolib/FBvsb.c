a6fcaf1bb1e868e1d71ceb0a10d1522d138a9b2e
Ryo Tanaka <tnkentre@mac.com>
2017-01-19 01:19:00 -0500

    Implemented put side of FBvsb.

--- audiolib/FBvsb.c
+++ audiolib/FBvsb.c
@@ -31,23 +31,83 @@
 #include "Registers.h"
 #include "FixedHeap.h"
 #include "vectors.h"
+#include "vectors_cplx.h"
 #include "cli.h"
 #include "FB.h"
 #include "FB_filters.h"
 #include "RevoFFT.h"
 #include "FBvsb.h"
 
+/************************************
+ *    Implementation of SimpleCB    *
+ ************************************/
+typedef struct {
+  int len;
+  int idx;
+  float * restrict buf;
+} SimpleCB;
+
+static SimpleCB* simplecb_init(int len)
+{
+  SimpleCB* st;
+  st = MEM_ALLOC(MEM_SDRAM, SimpleCB, 1, 4);
+  st->len = len;
+  st->idx = 0;
+  st->buf = MEM_ALLOC(MEM_SDRAM, float, len, 8);
+  return st;
+}
+
+static void simplecb_write(SimpleCB* restrict st, const float * buf, int len)
+{
+  int writelen;
+  int srcidx = 0;
+  while (len) {
+    writelen = MIN(st->len - st->idx, len);
+    COPY(&st->buf[st->idx], &buf[srcidx], writelen);
+    len     -= writelen;
+    srcidx  += writelen;
+    st->idx += writelen;
+    if (st->idx >= st->len) {
+      st->idx -= st->len;
+    }
+  }
+}
+
+static void simplecb_read(SimpleCB* restrict st, float * restrict buf, int len, int delay)
+{
+  int readlen;
+  int srcidx = ((1 + (len+delay) / st->len) * st->len + st->idx - (len+delay)) % st->len;
+  int dstidx = 0;
+  while (len) {
+    readlen = MIN(st->len - srcidx, len);
+    COPY(&buf[dstidx], &st->buf[srcidx], readlen);
+    len     -= readlen;
+    dstidx  += readlen;
+    srcidx  += readlen;
+    if (srcidx >= st->len) {
+      srcidx -= st->len;
+    }
+  }
+}
+
+/************************************
+ *    Implementation of FBVSB       *
+ ************************************/
 struct FBVSB_State_ {
   AC_HEADER(0,0);  
   /* configuration and history */
   int    fs;
   int    frame_size;
+  int    fft_size;
   int    fbfilter_size;
   int    nband;
   int    nfbbuf;
 
   rv_complex ** fbbuf[2];
-  float* tdbuf[2];
+  rv_complex ** phbuf[2];
+  SimpleCB   *  tdbuf[2];
+  float      *  ha;
+  float      *  hs;
   int    ipos;
   float  fpos;
 
@@ -68,6 +128,12 @@ FBVSB_State *FBvsb_init(const char * name, int fs, int frame_size, float length_
 {
   int ch, i;
   FBVSB_State* st;
+  const FB_filters_info * fbinfo;
+
+  int fft_size      = 2 * frame_size;
+  int fbfilter_size = 2 * fft_size;
+  int nband         = fft_size / 2;
+  
   st = MEM_ALLOC(MEM_SDRAM, FBVSB_State, 1, 8);
 
   AC_ADD(name, CLI_RLSL_FE, st, "FBVSB");
@@ -75,24 +141,103 @@ FBVSB_State *FBvsb_init(const char * name, int fs, int frame_size, float length_
 
   st->fs            = fs;
   st->frame_size    = frame_size;
-  st->fbfilter_size = 4 * frame_size;
-  st->nband         = frame_size;
-  st->nfbbuf        = (int)(length_sec * st->fs / st->frame_size);
+  st->fft_size      = fft_size;
+  st->fbfilter_size = fbfilter_size;
+  st->nband         = nband;
+  st->nfbbuf        = (int)(length_sec * fs / frame_size);
   for (ch = 0; ch < 2; ch++) {
     st->fbbuf[ch] = MEM_ALLOC(MEM_SDRAM, rv_complex*, st->nfbbuf, 8);
+    st->phbuf[ch] = MEM_ALLOC(MEM_SDRAM, rv_complex*, st->nfbbuf, 8);
     for (i = 0; i < st->nfbbuf; i++) {
-      st->fbbuf[ch][i] = MEM_ALLOC(MEM_SDRAM, rv_complex, st->nband+1, 8);
+      st->fbbuf[ch][i] = MEM_ALLOC(MEM_SDRAM, rv_complex, nband+1, 8);
+      st->phbuf[ch][i] = MEM_ALLOC(MEM_SDRAM, rv_complex, nband+1, 8);
     }
-    st->tdbuf[ch] = MEM_ALLOC(MEM_SDRAM, float, st->fbfilter_size, 8);
+    st->tdbuf[ch] = simplecb_init(fbfilter_size + frame_size);
   }
+
+  fbinfo = FB_filters_getinfo(frame_size, fft_size, fbfilter_size);
+  st->ha = MEM_ALLOC(MEM_SDRAM, float, fbfilter_size, 8);
+  st->hs = MEM_ALLOC(MEM_SDRAM, float, fbfilter_size, 8);
+  COPY(st->ha, fbinfo->filter_h, fbfilter_size);
+  COPY(st->hs, fbinfo->filter_g, fbfilter_size);
+
+  st->fft           = RevoFFT_initr(fft_size);
+  
   st->ipos          = 0;
   st->fpos          = 0.0f;
   st->loopgain      = 1.0f;
   st->feedbackgain  = 1.0f;
-
-  st->fft           = RevoFFT_initr(2 * st->nband);
 	
   return st;
+}
+
+static void proc_put(FBVSB_State * restrict st, float* src[], float* speed)
+{
+  int i,m,ch;
+  int frame_size    = st->frame_size;
+  int fbfilter_size = st->fbfilter_size;
+  int fft_size      = st->fft_size;
+  int nband         = st->nband;
+  int cur           = (int)(st->fpos / frame_size);
+  int putlen        = 0;
+
+  VARDECLR(float,      fftbuf);
+  VARDECLR(rv_complex, fdtemp0);
+  VARDECLR(rv_complex, fdtemp1);
+  VARDECLR(float,      sctemp0);
+  VARDECLR(float,      sctemp1);
+  SAVE_STACK;
+  ALLOC(fftbuf,  fbfilter_size, float);
+  ALLOC(fdtemp0, nband+1,  rv_complex);
+  ALLOC(fdtemp1, nband+1,  rv_complex);
+  ALLOC(sctemp0, nband+1,  float);
+  ALLOC(sctemp1, nband+1,  float);
+  
+  for (i=0; i<frame_size; i++) {
+    st->fpos += speed[i];
+    putlen++;
+    if ((int)(st->fpos / frame_size) != cur) {
+      for (ch=0; ch<2; ch++) {
+	simplecb_write(st->tdbuf[ch], &src[ch][i-putlen+1], putlen);
+	// FB analysis
+	simplecb_read(st->tdbuf[ch], fftbuf, fbfilter_size, 0);
+	vec_mul1(fftbuf, st->ha, fbfilter_size);
+	for (m=0; m<fbfilter_size/fft_size-1; m++) {
+	  vec_add1(fftbuf, &fftbuf[(m+1)*fft_size], fft_size);
+	}
+	RevoFFT_fftr(st->fft, (float*)st->fbbuf[ch][cur], fftbuf);
+	// phase analysis
+	simplecb_read(st->tdbuf[ch], fftbuf, fbfilter_size, frame_size);
+	vec_mul1(fftbuf, st->ha, fbfilter_size);
+	for (m=0; m<fbfilter_size/fft_size-1; m++) {
+	  vec_add1(fftbuf, &fftbuf[(m+1)*fft_size], fft_size);
+	}
+	RevoFFT_fftr(st->fft, (float*)fdtemp0, fftbuf);
+	vec_cplx_mul_conj(fdtemp1, fdtemp0, st->fbbuf[ch][cur], nband);
+	vec_spectral_power(sctemp0, fdtemp1, nband);
+	vec_sqrt(sctemp1, sctemp0, nband);
+	vec_add1s(sctemp1, FLT_MIN, nband);
+	vec_inv(sctemp0, sctemp1, nband);
+	vec_cplx_mulr(st->phbuf[ch][cur], fdtemp1, sctemp0, nband);
+	// !!todo!! adjust fractional delay
+      }
+      putlen = 0;
+      cur = (int)(st->fpos / frame_size);
+      if (cur >= st->nfbbuf) {
+	st->fpos -= st->nfbbuf * frame_size;
+	cur = (int)(st->fpos / frame_size);
+      }
+      if (cur < 0) {
+	st->fpos += st->nfbbuf * frame_size;
+	cur = (int)(st->fpos / frame_size);
+      }
+    }
+  }
+  for (ch=0; ch<2; ch++) {
+    simplecb_write(st->tdbuf[ch], &src[ch][i-putlen+1], putlen);
+  }
+
+  RESTORE_STACK;
 }
 
 void FBvsb_process(FBVSB_State * restrict st, float* dst[], float* src[], float* speed, int len)
